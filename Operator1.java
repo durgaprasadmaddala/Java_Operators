aee952877e8e5ec600faf8853cfdf1a7137e2046
durgaprasadmaddala <durgaprasadmaddalaa@gmail.com>
2026-08-19 22:29:05 +0530

    Add files via upload

---
+++ Operator1.java
@@ -0,0 +1,18 @@
+package Operatorsinjava;
+
+public class Operator1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Assignment operators
+		int i=15;
+		 int j=25;
+		 
+		 System.out.println("Addition"+(i+j));  // Addition
+		 System.out.println("Subtraction"+(i-j));
+		 System.out.println("Multiplication"+(i*j));
+		 System.out.println("Division"+(i/j));
+		 System.out.println("Modulus"+(i%j));
+	}
+
+}
