847f04e9772e4705c631b489a3d605943330fe57
MADDALA DURGAPRASAD <durgaprasadmaddalaa@gmail.com>
2026-09-01 20:18:49 +0530

    Add files via upload

---
+++ On8.java
@@ -0,0 +1,18 @@
+package OneNine;
+
+public class On8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(5<<2);
+		
+		System.out.println(5>>2);
+
+		System.out.println(20<<1);
+		System.out.println(20>>1);
+
+
+	}
+
+}
---
+++ On9.java
@@ -0,0 +1,11 @@
+package OneNine;
+
+public class On9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(8>>>1);
+
+	}
+
+}
